14577dc0ba4c4b8fdff6992facf56444dc8af7ed
sena laptop rumah <diaryangel717@gmail.com>
2024-05-26 21:19:21 +0700

    update portofolio img

--- src/app/portofolio/page.jsx
+++ src/app/portofolio/page.jsx
@@ -4,51 +4,44 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useRef } from "react";
 
-// array  mapping items data
 const items = [
   {
     id: 1,
     color: "from-red-300 to-blue-300",
     title: "React Commerce",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    img: "/assets/ecommerse.jpg",
+    link: "/projects/react-commerce",
   },
   {
     id: 2,
     color: "from-blue-300 to-violet-300",
     title: "Next.js Medium Blog",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    img: "/assets/blog.jpg",
+    link: "/projects/nextjs-medium-blog",
   },
   {
     id: 3,
     color: "from-violet-300 to-purple-300",
     title: "Vanilla Book App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    img: "/assets/book.jpg",
+    link: "/projects/vanilla-book-app",
   },
   {
     id: 4,
     color: "from-purple-300 to-red-300",
     title: "Spotify Music App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    link: "https://lama.dev",
+    img: "/assets/spotify.jpg",
+    link: "/projects/spotify-music-app",
   },
 ];
 
 const PortofolioPage = () => {
-  // make hook animate portofolio moving scroll down and right
-
   const ref = useRef();
-  // scroll scrollYProgress
-  const { scrollYProgress } = useScroll({
-    target: ref,
-  });
-  // usetransform
+  const { scrollYProgress } = useScroll({ target: ref });
   const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
 
   return (
@@ -62,7 +55,6 @@ const PortofolioPage = () => {
         className="h-[600vh] relative"
         ref={ref}
       >
-        {/* TITLE view port*/}
         <div className="w-screen h-[calc(100vh-6rem)] flex flex-col gap-5 items-center justify-center">
           <div
             className="text-center"
@@ -97,7 +89,6 @@ const PortofolioPage = () => {
             ></path>
           </motion.svg>
         </div>
-        {/*  for scroll or shift to the right side view*/}
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div
             style={{ x }}
@@ -109,33 +100,31 @@ const PortofolioPage = () => {
                 className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
                 key={item.id}
               >
-                {/* ITEM CONTAINER */}
                 <div className="flex flex-col gap-4 text-white">
                   <h1 className="text-xl font-bold md:text-2xl lg:text-4xl xl:text-6xl">{item.title}</h1>
-                  {/* TO MAKE IMAGE VISIBLE CONFIGURATION */}
                   <div className="relative w-70 h-46 md:w-86 md:h-54 lg:w-[400px] lg:h-[250px] xl:w-[500px] xl:h-[320px]">
                     <Image
                       src={item.img}
                       alt=""
                       fill
+                      objectFit="cover"
                     />
                   </div>
-                  {/* ITEM DESCRIPTION */}
                   <p className="w-70 md:w86 lg:w-[300px] lg:text-lg xl:w-[400px]">{item.desc}</p>
-                  <Link
-                    href={item.link}
-                    className="flex justify-end"
-                  >
-                    {/* BUTTON CONFIGURATION */}
-                    <button className="text-sm md:p-4 md:text-md lg:p-4 lg:text-lg bg-white text-gray-600 font-semibold m-2 rounded">See Demo</button>
-                  </Link>
+                  {item.link && (
+                    <Link
+                      href={item.link}
+                      className="flex justify-end"
+                    >
+                      <button className="text-sm md:p-4 md:text-md lg:p-4 lg:text-lg bg-white text-gray-600 font-semibold m-2 rounded">See Demo</button>
+                    </Link>
+                  )}
                 </div>
               </div>
             ))}
           </motion.div>
         </div>
       </div>
-      {/* rounded container hire me link to contact page */}
       <div className="w-screen h-screen flex flex-col gap-10 items-center justify-center text-center mt-12">
         <h1 className="text-8xl mt-4">Do you have a project?</h1>
         <div className="relative">
@@ -143,7 +132,7 @@ const PortofolioPage = () => {
             animate={{ rotate: 360 }}
             transition={{ duration: 8, ease: "linear", repeat: Infinity }}
             viewBox="0 0 300 300"
-            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+            className="w-64 h-64 md:w-[500px] md:h-[500px]"
           >
             <defs>
               <path
@@ -172,5 +161,4 @@ const PortofolioPage = () => {
   );
 };
 
-// next js tidak mendukung domain dari EXTERNAL IMAGES  sehingga kita harus mengubahnya di next.config.
 export default PortofolioPage;
