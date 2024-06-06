5c2fe36db65f19a2b97ed2f8ffef729c92248c5b
sensei-sena <senajumena@gmail.com>
2024-06-06 10:08:40 +0700

    ubah form2

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -77,7 +77,7 @@ const ContactPage = () => {
         <form
           onSubmit={sendEmail}
           ref={form}
-          className="h-3/4 xs:h-3/4 sm:h-3/4 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-6 justify-center p-24 shadow-md"
+          className="h-3/4 xs:h-3/4 sm:h-3/4 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-6 justify-center p-24 xs:p-10 xs:gap-5 shadow-lg shadow-slate-500"
         >
           <span>Dear Sena Dev,</span>
           <textarea
